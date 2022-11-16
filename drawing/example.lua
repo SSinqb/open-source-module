23b9557a1b84996ca9c621f071dae2ae312c1bf2
weda <95471423+SSinqb@users.noreply.github.com>
2022-11-16 21:02:38 +0300

    Init

---
+++ drawing/example.lua
@@ -0,0 +1,22 @@
+--[[
+    weda's Drawing Module
+    Created on 11/16/22
+]]
+
+--[[
+    Drawing Module Functions : {
+        :new() -- makes a metatable for [drawingmod], put this on below the drawing mod loadstring. make a _G. variable for it named as "drawmod"
+        :create(<string> type,{<properties>}) -- creates drawing yes yes
+        :hide() -- hides all current drawings used
+        :deleteall() -- deletes all current drawings used
+    }
+]]
+
+--[[https://docs.synapse.to/docs/reference/drawing_lib.html]]-- [[Drawing Library Example]]
+
+local drawingmod = loadstring(game:HttpGet("https://raw.githubusercontent.com/SSinqb/open-source-module/main/drawing.lua"))()
+_G.drawmod = drawingmod:new()
+
+local circle = drawingmod:create("Circle",{ Visible = true,NumSides = 64,Radius = 300,Filled = false,Position = Vector2.new(600,200) })
+-- drawingmod:hide()
+-- drawingmod:deleteall()
