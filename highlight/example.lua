7e527fe0b7b5dc4ccef2a1599109f071bbd7b024
weda <95471423+SSinqb@users.noreply.github.com>
2022-11-17 12:51:33 +0300

    fixed example

--- highlight/example.lua
+++ highlight/example.lua
@@ -1,4 +1,4 @@
-local drawingmod = loadstring(game:HttpGet("https://raw.githubusercontent.com/SSinqb/open-source-module/main/drawing.lua"))()
+local drawingmod = loadstring(game:HttpGet("https://raw.githubusercontent.com/SSinqb/open-source-module/main/highlight/highlight.lua"))()
 _G.drawmod = drawingmod:new() -- this is needed
 
 local circle = drawingmod:create(game.Players.LocalPlayer,{ Enabled = true})
